4a59600097825a6576d4c12718df7054881f6f94
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2020-06-17 13:41:54 -0300

    Trabalhando com listas

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 var nome = "Marcus";
 var idade = 29;
-
 var msgPopup = "Bem vindo, " + nome + "!!!\n" + "Você tem " + idade + " anos...";
 
-alert(msgPopup);
-console.log(msgPopup);
+// alert(msgPopup);
+// console.log(msgPopup);
+
+var lista = ["maçã", "pêra", "laranja"];
+console.log(lista);
+
+lista.push("limão");
+console.log(lista);
+
+lista.pop();
+console.log(lista);
