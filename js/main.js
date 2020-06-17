f091c934ddde0929af2bc79b776d501d2635f868
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2020-06-17 19:55:11 -0300

    Trabalhando com funções

--- js/main.js
+++ js/main.js
@@ -36,11 +36,35 @@
 //     console.log(count);
 // }
 
-var d = new Date();
+// var d = new Date();
 
-dias = ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"]
-meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
+// dias = ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"]
+// meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
 
-console.log(d);
-console.log(dias[d.getDay()]);
-console.log(d.getDate() + " de " + meses[d.getMonth()] + " de " + d.getFullYear() )
+// console.log(d);
+// console.log(dias[d.getDay()]);
+// console.log(d.getDate() + " de " + meses[d.getMonth()] + " de " + d.getFullYear() )
+
+function soma(n1, n2){
+    return n1 + n2;
+}
+
+function setReplace( frase, nome, novo_nome ){
+    return frase.replace(nome, novo_nome);
+}
+
+function validaIdade(idade){
+    var validar;
+
+    if (idade >= 18 ){
+        validar = "Maior de idade";
+    } else {
+        validar = "Menor de idade";
+    }
+
+    return validar;
+}
+
+alert( soma(1,2) );
+alert( setReplace("Vai Japão", "Japão", "Brasil"));
+alert( validaIdade(prompt("Qual é a sua idade?")))
