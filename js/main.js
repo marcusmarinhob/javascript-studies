91be470e061e2ab5fa35e589baf96751604eab53
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2020-06-17 13:44:40 -0300

    Trabalhando com dicionário

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
-var nome = "Marcus";
-var idade = 29;
-var msgPopup = "Bem vindo, " + nome + "!!!\n" + "Você tem " + idade + " anos...";
+// var nome = "Marcus";
+// var idade = 29;
+// var msgPopup = "Bem vindo, " + nome + "!!!\n" + "Você tem " + idade + " anos...";
 
 // alert(msgPopup);
 // console.log(msgPopup);
 
-var lista = ["maçã", "pêra", "laranja"];
-console.log(lista);
+// var lista = ["maçã", "pêra", "laranja"];
+// console.log(lista);
 
-lista.push("limão");
-console.log(lista);
+// lista.push("limão");
+// console.log(lista);
 
-lista.pop();
-console.log(lista);
+// lista.pop();
+// console.log(lista);
+
+var fruta = {nome:"maça", cor: "verde"};
+console.log(fruta.nome);
+console.log(fruta.cor);
