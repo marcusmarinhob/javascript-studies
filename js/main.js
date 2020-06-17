c3fdc5cef5cec0e16baa6795a30f08d8dd623d1c
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2020-06-17 20:18:31 -0300

    Trabalhando com onload, onclick, onchange, onmouseover e onmouseout

--- js/main.js
+++ js/main.js
@@ -71,5 +71,20 @@
 
 function botao(){
     document.getElementById("agradecimento").innerHTML = "Você clicou no botão!!!";
+}
 
+function changeTextOver(element){
+    element.innerHTML = "Você está com o mouse aqui!";
+}
+
+function changeTextOut(element){
+    element.innerHTML = "Passe o mouse aqui...";
+}
+
+function load(){
+    alert("Página carregada!!!")
+}
+
+function funcChange(element){
+    alert("Valor " + element.value + " selecionado!")
 }
