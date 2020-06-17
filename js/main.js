b60e93a26adea64d9a65f06c6a190e1e2c929bd3
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2020-06-17 13:52:42 -0300

    Trabalhando com o while

--- js/main.js
+++ js/main.js
@@ -18,9 +18,16 @@
 // console.log(fruta.nome);
 // console.log(fruta.cor);
 
-var idade = prompt("Qual é a sua idade?");
-if(idade >= 18){
-    alert("Maior de idade!");
-} else {
-    alert("Menor de idade");
+// var idade = prompt("Qual é a sua idade?");
+// if(idade >= 18){
+//     alert("Maior de idade!");
+// } else {
+//     alert("Menor de idade");
+// }
+
+var count = 0;
+
+while (count < 5){
+    console.log(count);
+    count = count + 1;
 }
