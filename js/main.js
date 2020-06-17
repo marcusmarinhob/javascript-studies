30ab0751c55bd5ea2f5624e9a440b29c068498b5
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2020-06-17 20:03:42 -0300

    Trabalhando com botões

--- js/main.js
+++ js/main.js
@@ -45,26 +45,31 @@
 // console.log(dias[d.getDay()]);
 // console.log(d.getDate() + " de " + meses[d.getMonth()] + " de " + d.getFullYear() )
 
-function soma(n1, n2){
-    return n1 + n2;
+// function soma(n1, n2){
+//     return n1 + n2;
+// }
+
+// function setReplace( frase, nome, novo_nome ){
+//     return frase.replace(nome, novo_nome);
+// }
+
+// function validaIdade(idade){
+//     var validar;
+
+//     if (idade >= 18 ){
+//         validar = "Maior de idade";
+//     } else {
+//         validar = "Menor de idade";
+//     }
+
+//     return validar;
+// }
+
+// alert( soma(1,2) );
+// alert( setReplace("Vai Japão", "Japão", "Brasil"));
+// alert( validaIdade(prompt("Qual é a sua idade?")))
+
+function botao(){
+    document.getElementById("agradecimento").innerHTML = "Você clicou no botão!!!";
+
 }
-
-function setReplace( frase, nome, novo_nome ){
-    return frase.replace(nome, novo_nome);
-}
-
-function validaIdade(idade){
-    var validar;
-
-    if (idade >= 18 ){
-        validar = "Maior de idade";
-    } else {
-        validar = "Menor de idade";
-    }
-
-    return validar;
-}
-
-alert( soma(1,2) );
-alert( setReplace("Vai Japão", "Japão", "Brasil"));
-alert( validaIdade(prompt("Qual é a sua idade?")))
