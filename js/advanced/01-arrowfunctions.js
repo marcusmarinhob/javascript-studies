f4ab370fb5d2e5d28201b93a19ff4f5e56cf9da2
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2020-07-01 22:43:53 -0300

    Trabalhando com arrow functions

---
+++ js/advanced/01-arrowfunctions.js
@@ -0,0 +1,12 @@
+// Traditional function definition
+function log(value){
+    console.log(value);
+}
+
+// Annonymous function
+var log2 = function log2(value){
+    console.log(value);
+}
+
+// Execution
+log('test');
