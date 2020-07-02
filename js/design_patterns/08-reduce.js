7b3b58576ee5d6ddd3f7cc1e8872c126bb2a5dd6
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2020-07-01 21:18:27 -0300

    Trabalhando com reduce

---
+++ js/design_patterns/08-reduce.js
@@ -0,0 +1,44 @@
+const pets = [
+    {
+        type: 'dog',
+        name: 'bolinha',
+        age: 15,
+        weight: 30
+    },
+    {
+        type: 'cat',
+        name: 'mingal',
+        age: 6,
+        weight: 2
+    },
+    {
+        type: 'dog',
+        name: 'rex',
+        age: 4,
+        weight: 5
+    },
+    {
+        type: 'cat',
+        name: 'marrom',
+        age: 2,
+        weight: 1
+    },
+    {
+        type: 'fish',
+        name: 'gulp',
+        age: 1,
+        weight: 1
+    },
+    {
+        type: 'horse',
+        name: 'pé de pano',
+        age: 1,
+        weight: 1
+    },
+]
+
+const totalWeight = pets.reduce((total, pets)=>{
+    return total += pets.weight
+}, 0)
+
+console.log(totalWeight)
