45bc185d9f03fe3480c72e739ae679fbb39c1d2f
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2020-07-03 22:07:00 -0300

    BDD e TDD

---
+++ js/tests/bdd.js
@@ -0,0 +1,10 @@
+
+/**
+ * BDD (Behavior Driven Development)
+ * 
+ *  É um dos pilares do Extreme Programming.
+ * 
+ *  Técnica de desenvolvimento ágil que visa integrar regras de negócio
+ *  com linguagens de programação.
+ *   
+ * */ 
---
+++ js/tests/tdd.js
@@ -0,0 +1,13 @@
+
+/**
+ * TDD (Test Driven Development)
+ * 
+ *  É um dos pilares do Extreme Programming.
+ * 
+ *  Consiste em testar e refatorar em pequenos ciclos,
+ *  onde você escreve o teste antes do código,
+ *  faz o mesmo passar e refatorar o código
+ *   
+ *  Escrita do teste -> Escrita do código -> Refatoração
+ * 
+ * */ 
