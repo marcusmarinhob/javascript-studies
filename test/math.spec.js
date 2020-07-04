df3efa393d9182e105c4b93fccd32154acd457b5
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2020-07-03 22:40:38 -0300

    Trabalhando com hooks

--- test/math.spec.js
+++ test/math.spec.js
@@ -1,7 +1,13 @@
 const assert = require('assert');
 const Math = require('../src/math.js')
 
+let value = 0;
+
 describe('Math class', function(){
+    beforeEach(function() {
+        value = 0;
+    });
+
     it('Sum two numbers', function(){
         const math = new Math();
 
