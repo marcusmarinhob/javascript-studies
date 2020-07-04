be1fd697d694f850d81015d3e6d8e9e81a7f1fee
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2020-07-03 22:52:42 -0300

    Espionando execução de funções com o sinon

--- src/math.js
+++ src/math.js
@@ -2,6 +2,11 @@ class Math {
     sum(a, b) {
         return a + b;
     }
+
+    printSum(req, res, a, b){
+        res.load('index', a + b);
+    }
+
 }
 
 module.exports = Math;
--- test/math.spec.js
+++ test/math.spec.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 const Math = require('../src/math.js')
 const expect = require('chai').expect;
+const sinon = require('sinon');
 
 let value = 0;
 
@@ -16,4 +17,16 @@ describe('Math class', function(){
     });
 
     it('Multiply two numbers');
+
+    it.only('Calls req with sum and index values', function(){
+        const req = {};
+        const res = {
+            load: sinon.spy()
+        };
+        const math = new Math();
+
+        math.printSum(req, res, 5, 5);
+
+        expect(res.load.calledOnce).to.be.true;        
+    })
 });
